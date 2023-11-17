717bba9cb60d6a382bdad36982bb68e12e316666
MeeskaMooska <77514959+MeeskaMooska@users.noreply.github.com>
2023-11-17 13:13:04 -0500

    Token is now verified by matching id and token- db

--- serverless/verifyToken.js
+++ serverless/verifyToken.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const cookie = require('cookie');
 const jwtSecret = process.env.JWT_SECRET;
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 // Start of the function
 exports.handler = async (event) => {
@@ -21,14 +23,22 @@ exports.handler = async (event) => {
     const userSessionToken = cookie.parse(cookieHeader)['user_session_token'];
 
     try {
-        // Verify token
+        // Verify token is valid
         const decoded = jwt.verify(userSessionToken, jwtSecret);
+
+		await prisma.achievoToken.findFirst({
+			where: {
+				associated_user_id: decoded.id,
+				jwt_token: userSessionToken
+			}
+		})
         
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Token verified.', username: decoded.username }),
         };
     } catch (err) {
+		console.error(err);
         return {
             statusCode: 401,
             body: JSON.stringify({ error: 'Invalid token.', err }),
